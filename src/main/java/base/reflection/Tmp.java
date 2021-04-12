8abce9cff8a0127a38152df398fdb3382734f478
xiaomifeng <tianyashuibin@gmail.com>
2021-04-12 09:01:56 +0800

    upadte

---
+++ src/main/java/base/reflection/Tmp.java
@@ -0,0 +1,11 @@
+package base.reflection;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Tmp {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+    }
+}
