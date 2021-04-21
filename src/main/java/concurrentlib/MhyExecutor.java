b3def601d9f219b854ee5ae019aaeeb5efc3c71f
xiaomifeng <tianyashuibin@163.com>
2021-04-21 08:34:31 +0800

    update

--- src/main/java/concurrentlib/MhyExecutor.java
+++ src/main/java/concurrentlib/MhyExecutor.java
@@ -1,5 +1,7 @@
 package concurrentlib;
 
+import javax.validation.constraints.NotNull;
+
 // java.util.concurrent.Executor
 public interface MhyExecutor {
     /**
@@ -8,5 +10,5 @@ public interface MhyExecutor {
      * @throws java.util.concurrent.RejectedExecutionException if the task cannot be accepted
      * @throws NullPointerException if the command is null
      */
-    void execute(Runnable command);
+    void execute(@NotNull Runnable command);
 }
