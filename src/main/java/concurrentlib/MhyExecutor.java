c90ff5e212f8d7592919df6925f28469c25dbba1
xiaomifeng <tianyashuibin@163.com>
2021-04-21 08:24:34 +0800

    update

--- src/main/java/concurrentlib/MhyExecutor.java
+++ src/main/java/concurrentlib/MhyExecutor.java
@@ -1,12 +1,12 @@
 package concurrentlib;
 
 // java.util.concurrent.Executor
-public interface MhyExe {
+public interface MhyExecutor {
     /**
      * Execute the given command
      * @param command the runnable task
      * @throws java.util.concurrent.RejectedExecutionException if the task cannot be accepted
      * @throws NullPointerException if the command is null
      */
-    void exe(Runnable command);
+    void execute(Runnable command);
 }
